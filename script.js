d8155df32dd6a498a790e55d0dda179555b9a4bc
Niko-linux <orlov.icloud@mail.ru>
2023-03-23 12:44:38 +0600

    x

--- script.js
+++ script.js
@@ -2,9 +2,5 @@ const swiper = new Swiper('.swiper', {
     // Optional parameters
     loop: true,
   
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
+ 
   });
